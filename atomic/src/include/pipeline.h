fe91eb1cd784dbafad082ad372b7916345bf1612
Nikolay Bobovnikov <b.nikolay@dilyasoft.pro>
2023-03-23 07:41:26 +0300

    project setup

---
+++ atomic/src/include/pipeline.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+#include <memory>
+
+namespace Pipeline
+{
+}
---
+++ atomic/test/source/test.cpp
@@ -0,0 +1,22 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest.h"
+#include "pipeline.h"
+#include <iostream>
+
+using namespace Pipeline;
+
+/*
+TEST_CASE("Default value")
+{
+    Id id;
+    CHECK(id.get() == "A1");
+}
+
+
+TEST_CASE("Max id")
+{
+    Id id = "Z9-Z9-Z9-Z9-Z9-Z9-Z9-Z9-Z9-Z9";
+    CHECK_THROWS(id.advance());
+}
+*/
