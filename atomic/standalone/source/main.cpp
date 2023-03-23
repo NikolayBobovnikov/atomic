3b597c883bb3c8e62abec96c122c93ea24e5862d
Nikolay Bobovnikov <b.nikolay@dilyasoft.pro>
2023-03-23 09:08:14 +0300

    add parsing cli options

--- atomic/standalone/source/main.cpp
+++ atomic/standalone/source/main.cpp
@@ -9,7 +9,36 @@ inputs:
     - if not specified, use default name with timestamp
 */
 
-int main()
+#include <string>
+#include <iostream>
+#include <cxxopts.hpp>
+
+
+int main(int argc, char *argv[])
 {
+    using namespace std;
+
+    cxxopts::Options options("Pipeline", "Applies pipeline transformations of the input data.");
+
+    // define available options
+    options.add_options()
+        //("d,debug", "Enable debugging") // a bool parameter
+        //("i,integer", "Int param", cxxopts::value<int>())
+        ("c,config", "Pipeline settings JSON configuration file", cxxopts::value<string>())
+        ("v,verbose", "Verbose output", cxxopts::value<bool>()->default_value("false"))
+        ;
+
+    // parse options
+    auto result = options.parse(argc, argv);
+
+    try{
+        // get json pipeline config
+        auto pipelint = result["config"].as<string>();
+        cout << "Using settings " << pipelint << endl;
+    }
+    catch(exception& e){
+        cout << e.what() << endl;
+    }
+    
     return 0;
 }
