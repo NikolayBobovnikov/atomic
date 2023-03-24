0dfcc58b4f4a784e7e2d1082989dfcb8259186b2
Nikolay Bobovnikov <b.nikolay@dilyasoft.pro>
2023-03-24 22:06:54 +0300

    bugfix

--- atomic/src/include/pipeline.h
+++ atomic/src/include/pipeline.h
@@ -21,10 +21,10 @@ namespace Quant
     data_t process(data_t data) const override;
 
     void add(std::unique_ptr<Task> task);
-    void add(const std::type_info& input_type,
-            const type_info& output_type,
-            std::string task_name,
-            TaskParameters task_params);
+    void add(const std::type_info &input_type,
+             const std::type_info &output_type,
+             std::string task_name,
+             TaskParameters task_params);
 
   private:
     std::vector<std::unique_ptr<Task>> tasks;
